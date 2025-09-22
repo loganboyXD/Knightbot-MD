e7dd3d3e5af4e427431312f75e82f9ccd2310354
loganboyXD <solomonslogan7@gmail.com>
2025-09-22 16:58:31 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'RE CLAN', // Your name
-  ownerNumber: '27788868252', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '0788868252', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
